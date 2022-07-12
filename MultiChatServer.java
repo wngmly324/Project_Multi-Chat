5e0f208160b49f024a78d31c8790aa678bbf23fa
wngmly324 <103935831+wngmly324@users.noreply.github.com>
2022-07-12 21:58:42 +0900

    add ChatThread

--- MultiChatServer.java
+++ MultiChatServer.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.logging.Logger;
+
+import com.google.gson.Gson;
 
 public class MultiChatServer {
 	// 서버 소켓 및 클라이언트 연결 소켓
@@ -43,7 +47,56 @@ public class MultiChatServer {
 		
 	}
 	
+	// 연결된 모든 클라이언트에 메시지 중계
+	void msgSendAll(String msg) {
+		for(ChatThread ct : chatThreads) {
+			ct.outMsg.println(msg);
+		}
+	}
+	
 	class ChatThread extends Thread {
+		// 수신 메시지 및 파싱 메시지 처리를 위한 변수 선언
+		String msg;
 		
+		// 메시지 객체 생성
+		Message m = new Message();
+		
+		// Json 파서 초기화
+		Gson gson = new Gson();
+		
+		// 입출력 스트림
+		private BufferedReader inMsg = null;
+		private PrintWriter outMsg = null;
+		
+		public void run() {
+			// 상태 정보가 true이면 루프를 돌면서 사용자에게서 수신된 메시지 처리
+			while(status) {
+				msg = inMsg.readLine();
+				
+				// JSON 메시지를 Message 객체로 매핑
+				m = gson.fromJson(msg, Message.class);
+				
+				// 파싱된 문자열 배열의 두 번째 요소값에 따라 처리
+				// 로그아웃 메시지일 때
+				if(m.getType().equals("logout")) {
+					chatThreads.remove(this);
+					msgSendAll(gson.toJson(new Message(m.getId(), "", "님이 종료했습니다.", "server")));
+					//해당 클라이언트 스레드 종료로 status를 false로 설정
+					status = false;
+				}
+				// 로그인 메시지일 때
+				else if(m.getType().equals("login")) {
+					msgSendAll(gson.toJson(new Message(m.getId(), "", "님이 로그인했습니다.", "server")));
+				}
+				// 그 밖의 경우, 즉 일반 메시지일 때
+				else {
+					msgSendAll(msg);
+				}
+			}
+			
+			// 루프를 벗어나면 클라이언트 연결이 종료되므로 스레드 인터럽트
+			this.interrupt();
+			logger.info(this.getName() + " 종료됨!!");
+		}
 	}
 }
